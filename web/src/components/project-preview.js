196a6f495f0958d7916a3414062d6a2a4704430c
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-03-24 22:34:23 +0000

    UPDATE: project cards

--- web/src/components/project-preview.js
+++ web/src/components/project-preview.js
@@ -10,23 +10,16 @@ import { responsiveTitle3 } from "./typography.module.css";
 function ProjectPreview(props) {
   return (
     <Link className={styles.root} to={`/project/${props.slug.current}`}>
-      <div className={styles.leadMediaThumb}>
-        {props.mainImage && props.mainImage.asset && (
-          <img
-            src={imageUrlFor(buildImageObj(props.mainImage))
-              .width(600)
-              .height(Math.floor((9 / 16) * 600))
-              .url()}
-            alt={props.mainImage.alt}
-          />
-        )}
-      </div>
+      <div className={styles.projectDetail}>
       <h3 className={cn(responsiveTitle3, styles.title)}>{props.title}</h3>
       {props._rawExcerpt && (
         <div className={styles.excerpt}>
           <BlockText blocks={props._rawExcerpt} />
         </div>
       )}
+      </div>
+
+      
     </Link>
   );
 }
